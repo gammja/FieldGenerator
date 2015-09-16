14d716c517dc20db4e22f17f6f26998028dcacff
sp <some@gmail.com>
2015-09-16 23:26:17 +0600

    -

--- FieldGenerator/Program.cs
+++ FieldGenerator/Program.cs
@@ -10,187 +10,62 @@ namespace FieldGenerator
     {
         static void Main()
         {
+            var targetClass = new CodeTypeDeclaration("CodeDOMCreatedClass");
+            targetClass.IsClass = true;
+            targetClass.TypeAttributes = TypeAttributes.Public | TypeAttributes.Sealed;
+
             var targetUnit = new CodeCompileUnit();
-            var ns = new CodeNamespace("CodeDOMSample");
-            ns.Imports.Add(new CodeNamespaceImport("System"));
-            CodeTypeDeclaration targetClass = new CodeTypeDeclaration("CodeDOMCreatedClass")
-            {
-                IsClass = true,
-                TypeAttributes = TypeAttributes.Public | TypeAttributes.Sealed
-            };
-            ns.Types.Add(targetClass);
+            var ns = GenerateNameSpace(targetClass);
             targetUnit.Namespaces.Add(ns);
 
-            AddFields(targetClass);
-            AddProperties(targetClass);
-            AddConstructor(targetClass);
-            AddMethod(targetClass);
-            AddEntryPoint(targetClass);
+            var field = GenerateField("_width", typeof(double));
+            targetClass.Members.Add(field);
+
+            var property = GenerateProperty(field);
+            targetClass.Members.Add(property);
+
             GenerateCSharpCode(targetUnit, "SampleCode.cs");
         }
 
-        private static void AddFields(CodeTypeDeclaration targetClass)
+        private static CodeNamespace GenerateNameSpace(CodeTypeDeclaration targetClass)
+        {
+            var ns = new CodeNamespace("CodeDOMSample");
+            ns.Imports.Add(new CodeNamespaceImport("System"));
+            ns.Types.Add(targetClass);
+            return ns;
+        }
+
+        private static CodeMemberField GenerateField(string name, Type type)
         {
             CodeMemberField widthValueField = new CodeMemberField();
             widthValueField.Attributes = MemberAttributes.Private;
-            widthValueField.Name = "widthValue";
-            widthValueField.Type = new CodeTypeReference(typeof(System.Double));
-            widthValueField.Comments.Add(new CodeCommentStatement("The width of the object."));
-            targetClass.Members.Add(widthValueField);
-
-            CodeMemberField heightValueField = new CodeMemberField();
-            heightValueField.Attributes = MemberAttributes.Private;
-            heightValueField.Name = "heightValue";
-            heightValueField.Type = new CodeTypeReference(typeof(System.Double));
-            heightValueField.Comments.Add(new CodeCommentStatement("The height of the object."));
-            targetClass.Members.Add(heightValueField);
+            widthValueField.Name = name;
+            widthValueField.Type = new CodeTypeReference(type);
+            return widthValueField;
         }
 
-        private static void AddProperties(CodeTypeDeclaration targetClass)
+        private static CodeTypeMember GenerateProperty(CodeMemberField field)
         {
-            // Declare the read-only Width property.
             CodeMemberProperty widthProperty = new CodeMemberProperty();
             widthProperty.Attributes = MemberAttributes.Public | MemberAttributes.Final;
-            widthProperty.Name = "Width";
+            widthProperty.Name = GetName(field);
             widthProperty.HasGet = true;
-            widthProperty.Type = new CodeTypeReference(typeof(System.Double));
-            widthProperty.Comments.Add(new CodeCommentStatement("The Width property for the object."));
+            widthProperty.Type = field.Type;
             widthProperty.GetStatements.Add(new CodeMethodReturnStatement(
-                new CodeFieldReferenceExpression(
-                    new CodeThisReferenceExpression(), "widthValue")));
-            targetClass.Members.Add(widthProperty);
-
-            // Declare the read-only Height property.
-            CodeMemberProperty heightProperty = new CodeMemberProperty();
-            heightProperty.Attributes = MemberAttributes.Public | MemberAttributes.Final;
-            heightProperty.Name = "Height";
-            heightProperty.HasGet = true;
-            heightProperty.Type = new CodeTypeReference(typeof(System.Double));
-            heightProperty.Comments.Add(new CodeCommentStatement("The Height property for the object."));
-            heightProperty.GetStatements.Add(new CodeMethodReturnStatement(
-                new CodeFieldReferenceExpression(
-                    new CodeThisReferenceExpression(), "heightValue")));
-            targetClass.Members.Add(heightProperty);
-
-            // Declare the read only Area property.
-            CodeMemberProperty areaProperty = new CodeMemberProperty();
-            areaProperty.Attributes = MemberAttributes.Public | MemberAttributes.Final;
-            areaProperty.Name = "Area";
-            areaProperty.HasGet = true;
-            areaProperty.Type = new CodeTypeReference(typeof(System.Double));
-            areaProperty.Comments.Add(new CodeCommentStatement("The Area property for the object."));
-
-            // Create an expression to calculate the area for the get accessor  
-            // of the Area property.
-            CodeBinaryOperatorExpression areaExpression =
-                new CodeBinaryOperatorExpression(
-                    new CodeFieldReferenceExpression(
-                        new CodeThisReferenceExpression(), "widthValue"),
-                    CodeBinaryOperatorType.Multiply,
-                    new CodeFieldReferenceExpression(
-                        new CodeThisReferenceExpression(), "heightValue"));
-            areaProperty.GetStatements.Add(new CodeMethodReturnStatement(areaExpression));
-            targetClass.Members.Add(areaProperty);
+                new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), field.Name)));
+            return widthProperty;
         }
 
-        /// <summary> 
-        /// Adds a method to the class. This method multiplies values stored  
-        /// in both fields. 
-        /// </summary>
-        /// <param name="targetClass"></param> 
-        private static void AddMethod(CodeTypeDeclaration targetClass)
+        private static string GetName(CodeMemberField field)
         {
-            // Declaring a ToString method
-            CodeMemberMethod toStringMethod = new CodeMemberMethod();
-            toStringMethod.Attributes = MemberAttributes.Public | MemberAttributes.Override;
-            toStringMethod.Name = "ToString";
-            toStringMethod.ReturnType = new CodeTypeReference(typeof(System.String));
+            var name = field.Name;
+            if (string.IsNullOrWhiteSpace(name)) return string.Empty;
 
-            CodeFieldReferenceExpression widthReference =
-                new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), "Width");
-            CodeFieldReferenceExpression heightReference =
-                new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), "Height");
-            CodeFieldReferenceExpression areaReference =
-                new CodeFieldReferenceExpression(new CodeThisReferenceExpression(), "Area");
+            if (name.StartsWith("_")) name = name.Substring(1);
+            if (name.Length == 1) return name.ToUpper();
 
-            // Declaring a return statement for method ToString.
-            CodeMethodReturnStatement returnStatement = new CodeMethodReturnStatement();
-
-            // This statement returns a string representation of the width, 
-            // height, and area. 
-            string formattedOutput = "The object:" + Environment.NewLine +
-                                     " width = {0}," + Environment.NewLine +
-                                     " height = {1}," + Environment.NewLine +
-                                     " area = {2}";
-            returnStatement.Expression =
-                new CodeMethodInvokeExpression(
-                    new CodeTypeReferenceExpression("System.String"), "Format",
-                    new CodePrimitiveExpression(formattedOutput),
-                    widthReference, heightReference, areaReference);
-            toStringMethod.Statements.Add(returnStatement);
-            targetClass.Members.Add(toStringMethod);
-        }
-
-        /// <summary> 
-        /// Add a constructor to the class. 
-        /// </summary>
-        /// <param name="targetClass"></param> 
-        private static void AddConstructor(CodeTypeDeclaration targetClass)
-        {
-            // Declare the constructor
-            CodeConstructor constructor = new CodeConstructor();
-            constructor.Attributes = MemberAttributes.Public | MemberAttributes.Final;
-
-            // Add parameters.
-            constructor.Parameters.Add(new CodeParameterDeclarationExpression(typeof (System.Double), "width"));
-            constructor.Parameters.Add(new CodeParameterDeclarationExpression(typeof (System.Double), "height"));
-
-            // Add field initialization logic
-            CodeFieldReferenceExpression widthReference =
-                new CodeFieldReferenceExpression(
-                    new CodeThisReferenceExpression(), "widthValue");
-            constructor.Statements.Add(new CodeAssignStatement(widthReference,
-                new CodeArgumentReferenceExpression("width")));
-            CodeFieldReferenceExpression heightReference =
-                new CodeFieldReferenceExpression(
-                    new CodeThisReferenceExpression(), "heightValue");
-            constructor.Statements.Add(new CodeAssignStatement(heightReference,
-                new CodeArgumentReferenceExpression("height")));
-            targetClass.Members.Add(constructor);
-        }
-
-        /// <summary> 
-        /// Add an entry point to the class. 
-        /// </summary>
-        /// <param name="targetClass"></param> 
-        private static void AddEntryPoint(CodeTypeDeclaration targetClass)
-        {
-            CodeEntryPointMethod start = new CodeEntryPointMethod();
-            CodeObjectCreateExpression objectCreate =
-                new CodeObjectCreateExpression(
-                    new CodeTypeReference("CodeDOMCreatedClass"),
-                    new CodePrimitiveExpression(5.3),
-                    new CodePrimitiveExpression(6.9));
-
-            // Add the statement: 
-            // "CodeDOMCreatedClass testClass =  
-            //     new CodeDOMCreatedClass(5.3, 6.9);"
-            start.Statements.Add(new CodeVariableDeclarationStatement(
-                new CodeTypeReference("CodeDOMCreatedClass"), "testClass",
-                objectCreate));
-
-            // Creat the expression: 
-            // "testClass.ToString()"
-            CodeMethodInvokeExpression toStringInvoke =
-                new CodeMethodInvokeExpression(
-                    new CodeVariableReferenceExpression("testClass"), "ToString");
-
-            // Add a System.Console.WriteLine statement with the previous  
-            // expression as a parameter.
-            start.Statements.Add(new CodeMethodInvokeExpression(
-                new CodeTypeReferenceExpression("System.Console"),
-                "WriteLine", toStringInvoke));
-            targetClass.Members.Add(start);
+            // to title case
+            return char.ToUpper(name[0]) + name.Substring(1);
         }
 
         private static void GenerateCSharpCode(CodeCompileUnit targetUnit, string fileName)
